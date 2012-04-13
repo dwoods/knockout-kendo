59a5bc46036e41f906dcede60a6e6366344602e5
Darryl Woods <dswoods@gmail.com>
2012-04-13 12:55:15 -0700

    First pass as more full-featured template hookups in widgetConfig.templatesa Added support for widgetOptions binding, to hold reusable widget configuration in the view model.

--- src/knockout-kendoGrid.js
+++ src/knockout-kendoGrid.js
@@ -1,10 +1,14 @@
 createBinding({
-    name: "kendoGrid",
-    defaultOption: DATA,
-    watch: {
-        data: function(value) {
-            ko.kendo.setDataSource(this, value);
-        }
-    },
-    templates: ["rowTemplate", "cellTemplate"]
+	name: "kendoGrid",
+	defaultOption: DATA,
+	watch: {
+		data: function (value) {
+			ko.kendo.setDataSource(this, value);
+		}
+	},
+	templates: [
+		"rowTemplate",
+		"cellTemplate",
+		{ "columns": ["template", "groupHeaderTemplate", "groupFooterTemplate"] }
+	]
 });
